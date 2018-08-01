7cbbebacf1c88c02ba6353cdcf33522423049ab7
Thomas Boerger <thomas@webhippie.de>
2018-08-01 22:12:47 +0200

    Fallback if checksum file do not exist

--- kleister-cli.rb
+++ kleister-cli.rb
@@ -13,13 +13,21 @@ class KleisterCli < Formula
 
   stable do
     url "https://dl.webhippie.de/kleister/cli/0.1.0/kleister-cli-0.1.0-darwin-10.6-amd64"
-    sha256 open("https://dl.webhippie.de/kleister/cli/0.1.0/kleister-cli-0.1.0-darwin-10.6-amd64.sha256").read.split(" ").first
+    sha256 begin
+      open("https://dl.webhippie.de/kleister/cli/0.1.0/kleister-cli-0.1.0-darwin-10.6-amd64.sha256").read.split(" ").first
+    rescue
+      nil
+    end
     version "0.1.0"
   end
 
   devel do
     url "https://dl.webhippie.de/kleister/cli/master/kleister-cli-master-darwin-10.6-amd64"
-    sha256 open("https://dl.webhippie.de/kleister/cli/master/kleister-cli-master-darwin-10.6-amd64.sha256").read.split(" ").first
+    sha256 begin
+      open("https://dl.webhippie.de/kleister/cli/master/kleister-cli-master-darwin-10.6-amd64.sha256").read.split(" ").first
+    rescue
+      nil
+    end
     version "master"
   end
 
